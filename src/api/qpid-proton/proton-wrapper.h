a50812eaa573e0240907b36d5e82adf3361aa191
Otavio R. Piske <angusyoung@gmail.com>
2016-06-26 10:53:21 +0200

    FIX: added missing license information to the top of some source code files

--- src/api/qpid-proton/proton-wrapper.h
+++ src/api/qpid-proton/proton-wrapper.h
@@ -1,23 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ Copyright 2016 Otavio Rodolfo Piske
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
  */
-
-/* 
- * File:   proton-wrapper.h
- * Author: otavio
- *
- * Created on May 30, 2016, 9:30 PM
- */
-
 #ifndef PROTON_WRAPPER_H
 #define PROTON_WRAPPER_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 
     
 #include "msgctxt.h"
@@ -35,8 +36,6 @@ extern "C" {
 msg_ctxt_t *proton_init(void *data);
 void proton_stop(msg_ctxt_t *ctxt);
 void proton_destroy(msg_ctxt_t *ctxt);
-
-
 
 void proton_send(msg_ctxt_t *ctxt, msg_content_loader content_loader);
 void proton_subscribe(msg_ctxt_t *ctxt, void *data);
