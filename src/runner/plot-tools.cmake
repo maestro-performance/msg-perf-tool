51741a050be357c170aacb81b9ce4e273e8b8c99
Otavio Rodolfo Piske <opiske@redhat.com>
2016-07-14 21:18:17 +0200

    UPD: removed the parser, since it's unused in the newer version

--- src/runner/plot-tools.cmake
+++ src/runner/plot-tools.cmake
@@ -1,10 +1,10 @@
 # In all project subdirectories
-set(plot_tools mpt-parse.sh mpt-runner.sh)
+set(runner_tools mpt-runner.sh)
 
 set(EXECUTABLE_OUTPUT_PATH ${CMAKE_BINARY_DIR}/../../build/target/bin)
 
 # ... then traverse subdirectories
-foreach(tool ${plot_tools}) 
+foreach(tool ${runner_tools}) 
     message("Copying script file ${CMAKE_CURRENT_SOURCE_DIR}/${tool} to runtime dir ${EXECUTABLE_OUTPUT_PATH}")
     file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/${tool} DESTINATION ${EXECUTABLE_OUTPUT_PATH})
 endforeach(tool)
