d436c140a494c0076fcf4d5c2ecc07aec9e0c0c2
Otavio R. Piske <angusyoung@gmail.com>
2016-12-31 18:03:21 +0100

    NEW: initial stubs for MQTT support

---
+++ src/api/paho-c/paho-context.h
@@ -0,0 +1,41 @@
+/**
+ Copyright 2016 Otavio Rodolfo Piske
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+#ifndef PAHO_CONTEXT_H
+#define PAHO_CONTEXT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include <log/gru_logger.h>
+
+#include <MQTTClient.h>
+
+typedef struct paho_ctxt_t_ { 
+    MQTTClient client;
+} paho_ctxt_t;
+    
+
+paho_ctxt_t *paho_context_init();
+void proton_context_destroy(paho_ctxt_t **ctxt);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PAHO_CONTEXT_H */
+
