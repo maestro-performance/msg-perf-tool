0f66322d04959e46b8404fce73f988c072a7eb91
Otavio R. Piske <angusyoung@gmail.com>
2017-01-01 15:51:01 +0100

    NEW: work in progress in the Paho initialization

--- src/api/paho-c/paho-wrapper.h
+++ src/api/paho-c/paho-wrapper.h
@@ -17,12 +17,26 @@
 #ifndef PAHO_WRAPPER_H
 #define PAHO_WRAPPER_H
 
+#include <network/gru_uri.h>
+
+#include "contrib/options.h"
+#include "msgctxt.h"
+#include "paho-context.h"
+#include "statistics.h"
+
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+msg_ctxt_t *paho_init(stat_io_t *stat_io, void *data);
+void paho_stop(msg_ctxt_t *ctxt);
+void paho_destroy(msg_ctxt_t *ctxt);
 
-
+void paho_send(msg_ctxt_t *ctxt, msg_content_loader content_loader);
+void paho_subscribe(msg_ctxt_t *ctxt, void *data);
+void paho_receive(msg_ctxt_t *ctxt, msg_content_data_t *content);
 
 #ifdef __cplusplus
 }
