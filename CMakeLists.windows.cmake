c1799d0f44e9aa1436e64f899818eae786c28ad0
Otavio R. Piske <angusyoung@gmail.com>
2017-01-28 17:03:33 +0100

    UPD: cleanup the build code and replaces the fixed locations with the ones provided by GNUInstallDirs

--- CMakeLists.windows.cmake
+++ CMakeLists.windows.cmake
@@ -1,9 +1,8 @@
-set(CPP_LIBRARY_DIR "lib")
-set(EXECUTABLE_OUTPUT_PATH ${CMAKE_BINARY_DIR}/target/bin)
-set(LIBRARY_OUTPUT_PATH ${CMAKE_BINARY_DIR}/target/lib)
-set (CTEST_BINARY_DIRECTORY ${CMAKE_BINARY_DIR}/target/lib)
+set(EXECUTABLE_OUTPUT_PATH ${CMAKE_BINARY_DIR}/target/${CMAKE_INSTALL_BINDIR})
+set(LIBRARY_OUTPUT_PATH ${CMAKE_BINARY_DIR}/target/${CMAKE_INSTALL_LIBDIR})
+set (CTEST_BINARY_DIRECTORY ${CMAKE_BINARY_DIR}/target/${CMAKE_INSTALL_LIBDIR})
 
 add_subdirectory(deps/uriparser)
-include_directories(${CMAKE_BINARY_DIR}/uriparser/include)
+include_directories(${CMAKE_BINARY_DIR}/uriparser/${CMAKE_INSTALL_INCLUDEDIR})
 
 set(URIPARSER_LIB "uriparser")
