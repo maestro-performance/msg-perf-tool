5286734ff9bdfb76d3753e83d4e221e460f3500d
Otavio Rodolfo Piske <opiske@redhat.com>
2017-05-03 20:13:23 +0200

    FIX: use Debug as the default build mode if unset

--- CMakeLists.default.cmake
+++ CMakeLists.default.cmake
@@ -11,6 +11,13 @@ set(RUNTIME_DIR "bin")
 set(CPP_INCLUDE_DIR "include")
 set(SHARED_DIR "share")
 
+if (NOT CMAKE_BUILD_TYPE)
+	set(CMAKE_BUILD_TYPE Debug CACHE STRING
+      	"Choose the type of build, options are: Debug Release."
+      	FORCE
+	)
+endif(NOT CMAKE_BUILD_TYPE)
+
 if (CMAKE_COMPILER_IS_GNUCXX)
         if (BUILD_WITH_EXTRA_DEBUG)
 			add_definitions(-DMPT_DEBUG=2)
