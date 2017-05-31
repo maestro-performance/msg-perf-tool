7fd772fd1f61f6f16064e6f07ff499dae320d27a
Otavio Rodolfo Piske <opiske@redhat.com>
2017-05-31 15:17:38 +0200

    DEL: unsed test code

--- test/common/maestro_serialize_test.c
+++ test/common/maestro_serialize_test.c
@@ -19,59 +19,6 @@
 #include "maestro/maestro_serialize.h"
 #include "maestro/maestro_deserialize.h"
 
-static void sample_stats(maestro_note_t *response) {
-	maestro_note_set_type(response, MAESTRO_TYPE_RESPONSE);
-	maestro_note_set_cmd(response, MAESTRO_NOTE_STATS);
-	maestro_note_stats_set_id(response, "07a549ac-d490-4cce-8b27-551cfe8c991f");
-	maestro_note_stats_set_name(response, "test@unit.test");
-
-	maestro_note_stats_set_child_count(response, 6);
-	maestro_note_stats_set_role(response, "receiver");
-	maestro_note_stats_set_roleinfo(response, "perf");
-	maestro_note_stats_set_stat_type(response, 'R');
-
-	gru_timestamp_t now = gru_time_now();
-	char *formatted_ts = gru_time_write_str(&now);
-
-	maestro_note_stats_set_perf_ts(response, formatted_ts);
-	gru_dealloc_string(&formatted_ts);
-
-	maestro_note_stats_set_perf_count(response, 100);
-	maestro_note_stats_set_perf_rate(response, 10);
-	maestro_note_stats_set_perf_latency(response, 11.2);
-}
-
-static int maestro_serialize_stats_test() {
-	maestro_note_t sample = {0};
-	gru_status_t status = gru_status_new();
-
-	if (!maestro_note_payload_prepare(&sample, &status)) {
-		fprintf(stderr, "%s\n", status.message);
-
-		return EXIT_FAILURE;
-	}
-
-	sample_stats(&sample);
-
-	msg_content_data_t content = {0};
-	msg_content_data_init(&content, MAESTRO_NOTE_SIZE, &status);
-	if (!gru_status_success(&status)) {
-		fprintf(stderr, "%s\n", status.message);
-
-		return EXIT_FAILURE;
-	}
-
-	maestro_serialize_note(&sample, &content);
-
-	printf("Wrote %"PRIu64" bytes (expected %d)\n", content.size, MAESTRO_NOTE_SIZE);
-	printf("Data: %-*s|||\n", MAESTRO_NOTE_SIZE, (char *) content.data);
-
-	msg_content_data_release(&content);
-	maestro_note_payload_cleanup(&sample);
-	return EXIT_SUCCESS;
-}
-
-
 static bool maestro_serialize_cmd_ping_request_test() {
 	gru_status_t status = gru_status_new();
 	maestro_note_t note = {0};
