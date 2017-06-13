e9eba5422415b4a0f85942d5ae1cdbc373d94e93
Otavio Rodolfo Piske <opiske@redhat.com>
2017-06-13 18:58:08 +0200

    NEW: initial work to implement handlers (WIP)

---
+++ src/common/vmslh.c
@@ -0,0 +1,44 @@
+/**
+ Copyright 2017 Otavio Rodolfo Piske
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#include "vmslh.h"
+
+bool vmslh_add(gru_list_t *list, vmslh_callback_fn callback, gru_status_t *status) {
+	vmslh_callback_t *wrapper = gru_alloc(sizeof(vmslh_callback_t), status);
+	gru_alloc_check(wrapper, false);
+
+	wrapper->call = callback;
+
+	if (!gru_list_append(list, wrapper)) {
+		gru_dealloc((void **)&wrapper);
+		gru_status_set(status, GRU_FAILURE, "Unable to append VMSL handler wrapper");
+
+		return false;
+	}
+
+	return true;
+}
+
+void vmslh_run(gru_list_t *list, void *ctxt, void *payload) {
+	gru_node_t *node = list->root;
+
+	while (node) {
+		vmslh_callback_t *callback = (vmslh_callback_t *) node->data;
+
+		callback->call(ctxt, payload);
+
+		node = node->next;
+	}
+}
---
+++ src/common/vmslh.h
@@ -0,0 +1,40 @@
+/**
+ Copyright 2017 Otavio Rodolfo Piske
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+#ifndef MPT_VMSLH_H
+#define MPT_VMSLH_H
+
+#include <common/gru_status.h>
+#include <common/gru_alloc.h>
+#include <collection/gru_list.h>
+
+typedef void (*vmslh_callback_fn)(void *ctxt, void *payload);
+
+typedef struct vmslh_callback_t_ {
+  vmslh_callback_fn call;
+} vmslh_callback_t;
+
+typedef struct vmslh_send_t_ {
+  gru_list_t *before;
+  gru_list_t *after;
+} vmslh_send_t;
+
+
+bool vmslh_add(gru_list_t *list, vmslh_callback_fn callback, gru_status_t *status);
+void vmslh_run(gru_list_t *list, void *ctxt, void *payload);
+
+
+#endif //MPT_VMSLH_H
