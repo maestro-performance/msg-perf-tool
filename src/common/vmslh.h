fe28367426c30101a3dad181c215cd04c0a9c071
Otavio Rodolfo Piske <opiske@redhat.com>
2017-07-22 09:03:17 +0200

    FIX: added missing documentation

--- src/common/vmslh.h
+++ src/common/vmslh.h
@@ -25,13 +25,24 @@
 extern "C" {
 #endif
 
+/**
+ * VMSL callback signature
+ */
 typedef void (*vmslh_callback_fn)(void *ctxt, void *msg, void *payload);
 
+
+/**
+ * VMSL callback object
+ */
 typedef struct vmslh_callback_t_ {
   vmslh_callback_fn call;
   void *payload;
 } vmslh_callback_t;
 
+/**
+ * A set of list of callbacks to be executed during different stages of messaging
+ * processing
+ */
 typedef struct vmslh_handlers_t_ {
   gru_list_t *before_connect;
   gru_list_t *after_connect;
@@ -42,12 +53,42 @@ typedef struct vmslh_handlers_t_ {
   gru_list_t *finalize_receive;
 } vmslh_handlers_t;
 
-
+/**
+ * Add or replace a callback to the VMSL handlers list
+ * @param list the list of VMSL handlers
+ * @param callback the call back to add/replace
+ * @param payload the payload to the callback
+ * @param status status structure
+ * @return true if successfully added/replaced or false otherwise
+ */
 bool vmslh_add(gru_list_t *list, vmslh_callback_fn callback, void *payload, gru_status_t *status);
+
+/**
+ * Removes a callback from the VMSL handlers list
+ * @param list the list of VMSL handlers
+ * @param callback the call back to remove
+ */
 void vmslh_remove(gru_list_t *list, vmslh_callback_fn callback);
+
+/**
+ * Runs a VMSL handlers list executing all the existent callbacks
+ * @param list the list to add
+ * @param ctxt the messaging context
+ * @param msg the current message being processed (sent/received/etc) in VMSL
+ */
 void vmslh_run(gru_list_t *list, void *ctxt, void *msg);
 
+/**
+ * Create a new VMSL handlers structure
+ * @param status status object
+ * @return a new handlers structure or false otherwise (details available in the status object)
+ */
 vmslh_handlers_t vmslh_new(gru_status_t *status);
+
+/**
+ * Cleans up the VMSL handlers structure
+ * @param handlers the structure to cleanup
+ */
 void vmslh_cleanup(vmslh_handlers_t *handlers);
 
 #ifdef __cplusplus
