45831348ee22b162d9c6ee789b6aa198bc0f95db
Otavio R. Piske <angusyoung@gmail.com>
2017-08-18 18:55:10 +0200

    DEL: the old tune sender program (replaced by maestro)

--- src/applications/sender/sender_main.c
+++ src/applications/sender/sender_main.c
@@ -15,30 +15,8 @@
  */
 #include "sender_main.h"
 
-void show_help() {
-	gru_cli_general_usage("mpt-sender");
-
-	gru_cli_program_description(
-		"perf", "Runs a performance test against a message broker");
-	gru_cli_program_description(
-		"tune", "Tries to automatically determine the sustained throughput");
-}
-
 int main(int argc, char **argv) {
-	if (argc < 2) {
-		show_help();
-
-		return EXIT_FAILURE;
-	} else {
-		if (strncmp(argv[1], "perf", 4) == 0) {
-			return perf_main((argc - 1), &argv[1]);
-		} else if (strncmp(argv[1], "--help", 6) == 0) {
-			show_help();
-		} else {
-			show_help();
-			return EXIT_FAILURE;
-		}
-	}
+	return perf_main(argc, argv);
 
 	return EXIT_SUCCESS;
 }
