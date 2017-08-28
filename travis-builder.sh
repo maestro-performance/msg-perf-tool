350aafa6dc397bccae79b29e3f523f459ebc8d46
Otavio Rodolfo Piske <opiske@redhat.com>
2017-08-28 11:29:08 +0200

    FIX: several Travis CI issues
    - overrides outdated msgpack package
    - return the correct error code on compilation issues
    - added comments

--- travis-builder.sh
+++ travis-builder.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+function travis_abort() {
+    echo "Unable to build $1"
+    exit 1
+}
+
 function before_install() {
     if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
         brew update
@@ -8,39 +13,65 @@ function before_install() {
 }
 
 function before_script() {
-    echo "Installing gru"
 
+    # The following dependencies are unavailable on apt repositories
+    echo "Installing gru"
     wget https://github.com/orpiske/gru/tarball/master -O gru-head.tar.gz
     mkdir gru && tar -xvf gru-head.tar.gz -C gru --strip-components=1
-    pushd gru && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=DEBUG .. && make && sudo make install ; popd
+    pushd gru && mkdir build && cd build
+    cmake -DCMAKE_BUILD_TYPE=DEBUG .. || travis_abort gru
+    make  || travis_abort gru
+    sudo make install  || travis_abort gru
+    popd
 
     echo "Installing bmic"
     wget https://github.com/orpiske/bmic/tarball/master -O bmic-head.tar.gz
     mkdir bmic && tar -xvf bmic-head.tar.gz -C bmic --strip-components=1
-    pushd bmic && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=DEBUG .. && make && sudo make install ; popd
+    pushd bmic && mkdir build && cd build
+    cmake -DCMAKE_BUILD_TYPE=DEBUG ..  || travis_abort bmic
+    make  || travis_abort bmic
+    sudo make install  || travis_abort bmic
+    popd
 
     echo "Installing litestomp"
     wget https://github.com/orpiske/litestomp/tarball/master -O litestomp-head.tar.gz
     mkdir litestomp && tar -xvf litestomp-head.tar.gz -C litestomp --strip-components=1
-    pushd litestomp && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=DEBUG .. && make && sudo make install ; popd
+    pushd litestomp && mkdir build && cd build
+    cmake -DCMAKE_BUILD_TYPE=DEBUG ..  || travis_abort litestomp
+    make  || travis_abort litestomp
+    sudo make install  || travis_abort litestomp
+    popd
 
     echo "Installing Paho MQTT C"
     wget https://github.com/eclipse/paho.mqtt.c/tarball/master -O pahoc-head.tar.gz
     mkdir pahoc && tar -xvf pahoc-head.tar.gz -C pahoc --strip-components=1
-    pushd pahoc && mkdir build.paho && cd build.paho && cmake -DPAHO_WITH_SSL=FALSE -DPAHO_BUILD_SAMPLES=FALSE -DPAHO_BUILD_DOCUMENTATION=FALSE -DCMAKE_BUILD_TYPE=RELEASE .. && make && sudo make install ; popd
-
+    pushd pahoc && mkdir build.paho && cd build.paho
+    cmake -DPAHO_WITH_SSL=FALSE -DPAHO_BUILD_SAMPLES=FALSE -DPAHO_BUILD_DOCUMENTATION=FALSE -DCMAKE_BUILD_TYPE=RELEASE ..  || travis_abort paho
+    make || travis_abort paho
+    sudo make install || travis_abort paho
+    popd
 
     echo "Installing Hdr Histogram C"
     wget https://github.com/HdrHistogram/HdrHistogram_c/tarball/master -O hdr-histogram-c-head.tar.gz
     mkdir hdr-histogram-c && tar -xvf hdr-histogram-c-head.tar.gz -C hdr-histogram-c --strip-components=1
     pushd hdr-histogram-c && mkdir build && cd build && cmake -DCMAKE_USER_C_FLAGS="-fPIC" .. && make && sudo make install ; popd
+
+    # The version of msgpack on TravisCI is ancient and won't work with this project
+    echo "Installing MsgPack C"
+    wget https://github.com/msgpack/msgpack-c/tarball/master -O msgpack-c-head.tar.gz
+    mkdir msgpack-c && tar -xvf msgpack-c-head.tar.gz -C msgpack-c --strip-components=1
+    pushd msgpack-c && mkdir build && cd build
+    cmake -DCMAKE_USER_C_FLAGS="-fPIC" .. || travis_abort msgpack
+    make || travis_abort msgpack
+    sudo make install || travis_abort msgpack
+    popd
 }
 
 function build() {
     mkdir -p build
     cd build
-    cmake -DCMAKE_BUILD_TYPE=DEBUG -DSTOMP_SUPPORT=OFF -DAMQP_SUPPORT=OFF ..
-    make all
+    cmake -DCMAKE_BUILD_TYPE=DEBUG -DSTOMP_SUPPORT=OFF -DAMQP_SUPPORT=OFF .. || travis_abort msg-perf-tool
+    make all || travis_abort msg-perf-tool
     cd ..
 }
 
